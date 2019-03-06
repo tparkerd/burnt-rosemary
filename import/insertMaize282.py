7ac0107b84b0c4d433e12342cbaf35654979a551
Tim Parker <Tim.ParkerD@gmail.com>
2019-03-06 11:49:52 -0600

    Started changing over from Sphinx-styled docstrings to Google-styled docstrings

--- import/insertMaize282.py
+++ import/insertMaize282.py
@@ -7,9 +7,9 @@ import numpy as np
 import psycopg2
 import csv
 import insert
-import find
+from util import find
 from dbconnect import config, connect
-from models import species, population, line, chromosome, variant, genotype, trait, phenotype, growout_type, growout, location, gwas_algorithm, genotype_version, imputation_method, kinship_algorithm, kinship, population_structure_algorithm, population_structure, gwas_run, gwas_result
+from util.models import species, population, line, chromosome, variant, genotype, trait, phenotype, growout_type, growout, location, gwas_algorithm, genotype_version, imputation_method, kinship_algorithm, kinship, population_structure_algorithm, population_structure, gwas_run, gwas_result
 
 
 if __name__ == '__main__':
